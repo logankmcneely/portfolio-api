95eea7caa4626c67c5b70243dc5622f819b389a0
Logan McNeely <logan.k.mcneely@gmail.com>
2020-08-24 09:23:47 -0500

    Initial Mongo implementation

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const server = express()
+const config = require('./config/dev')
+
+const mongoose = require('mongoose')
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  if (err) console.log(err)
+  else console.log('> Connected to DB')
+})
+
+const portfoliosRoutes = require('./routes/portfolios')
 
 server.get('/test', (req, res) => {
-  return res.json({message: 'test is working'})
+  return res.json({ message: 'test is working' })
 })
+
+server.use('/api/v1/portfolios', portfoliosRoutes)
 
 const PORT = process.env.PORT || 3001
 server.listen(PORT, (err) => {
