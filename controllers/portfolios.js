4a8d2c85644a8f78b74d51262b3b39351ee4efe0
Logan McNeely <logan.k.mcneely@gmail.com>
2020-08-24 10:53:19 -0500

    + getPortfolioById

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -5,3 +5,12 @@ exports.getPortfolios = async (req, res) => {
   const portfolios = await Portfolio.find({})
   return res.json(portfolios)
 }
+
+exports.getPortfolioById = async (req, res) => {
+  try {
+    const portfolio = await Portfolio.findById(req.params.id)
+    return res.json(portfolio)
+  } catch (e) {
+    return res.status(422).send(`API Error: ${e.message}`)
+  }
+}
