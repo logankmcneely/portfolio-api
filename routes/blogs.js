e2a905846939f03211a46a1e9eb3c41fa0c02173
Logan McNeely <logan.k.mcneely@gmail.com>
2020-09-01 09:42:06 -0500

    +createBlog

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,11 +9,14 @@ const {
 const { 
   getBlogs,
   getBlogById,
-  getBlogBySlug
+  getBlogBySlug,
+  createBlog
 } = require('../controllers/blogs')
 
 router.get('', getBlogs)
 router.get('/:id', getBlogById)
 router.get('/s/:slug', getBlogBySlug)
+
+router.post('', checkJwt, checkRole('admin'), createBlog)
 
 module.exports = router
